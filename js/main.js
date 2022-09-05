4f0dc797a7e71b4853acb44a6b7caea3da282f42
Muhammadbek <galaxylogin@yandex.com>
2022-09-05 22:13:12 +0500

    Added country's more info page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const elChangeThemeBtn = document.querySelector(".header-btn");
+var bg = document.querySelector(".hero--active");
 
 elChangeThemeBtn.addEventListener("click" , function(){
   document.body.classList.toggle("theme-dark");
+  bg.classList.toggle("bg-white");
 });
+
